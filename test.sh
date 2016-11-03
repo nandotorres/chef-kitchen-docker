5fc0b23fef2a26a0df652dfb7a07acdd2bbb3cb9
Fernando Torres <ftorres@avenuecode.com>
2016-11-03 10:00:59 -0200

    Fixing CI descriptor with service section

--- test.sh
+++ test.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+tag=$1
+
+if [[ -z "$tag" ]]; then
+  tag="latest"
+fi
+
 docker run -it registry.gitlab.com/fcagil/chef-docker-runner-image: /bin/bash -c "/opt/chefdk/bin/chef -v && docker -v"
 
 exit $?
